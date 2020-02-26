7aaf2ca82874a9c0e32e914f109f5c1bd40d4a33
sanka1 <sanka@xiges.io>
2020-02-26 17:32:04 +0530

    HandlersGO1

--- Go/src/handlers.go
+++ Go/src/handlers.go
@@ -2,22 +2,50 @@ package handlers
 
 import (
 	"encoding/json"
-	"github.com/SankaKodippily/golang-auth0-example/Go/src/"
-	"github.com/gin-gonic/gin"
 	"io"
 	"io/ioutil"
 	"net/http"
+	todo "github.com/SankaKodippily/golang-auth0-example/Go/src/"
+	"github.com/gin-gonic/gin"
 )
-func GetTodoListHandler(c* gin.Context) {
-c.JSON(http.StatusOK,todo.Get())
+
+// GetTodoListHandler returns all current todo items
+func GetTodoListHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, todo.Get())
 }
+
+// AddTodoHandler adds a new todo to the todo list
 func AddTodoHandler(c *gin.Context) {
-	todoItem, statusCode, err:= convertHTTPBodyToTodo(c.Request.Body)
+	todoItem, statusCode, err := convertHTTPBodyToTodo(c.Request.Body)
 	if err != nil {
-		c.JSON(statusCode,err)
+		c.JSON(statusCode, err)
 		return
 	}
-	c.JSON(statusCode,gin.H{"id": todo.Add(todoItem.Message)})
+	c.JSON(statusCode, gin.H{"id": todo.Add(todoItem.Message)})
+}
+
+// DeleteTodoHandler will delete a specified todo based on user http input
+func DeleteTodoHandler(c *gin.Context) {
+	todoID := c.Param("id")
+	if err := todo.Delete(todoID); err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	c.JSON(http.StatusOK, "")
+}
+
+// CompleteTodoHandler will complete a specified todo based on user http input
+func CompleteTodoHandler(c *gin.Context) {
+	todoItem, statusCode, err := convertHTTPBodyToTodo(c.Request.Body)
+	if err != nil {
+		c.JSON(statusCode, err)
+		return
+	}
+	if todo.Complete(todoItem.ID) != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	c.JSON(http.StatusOK, "")
 }
 
 func convertHTTPBodyToTodo(httpBody io.ReadCloser) (todo.Todo, int, error) {
@@ -35,14 +63,5 @@ func convertJSONBodyToTodo(jsonBody []byte) (todo.Todo, int, error) {
 	if err != nil {
 		return todo.Todo{}, http.StatusBadRequest, err
 	}
-	return todoItem,http.StatusOK,nil
+	return todoItem, http.StatusOK, nil
 }
-func DeleteTodoHandler(c* gin.Context){
-	todoID := c.Param("id")
-	if err := todo.Delete(todoID); err != nil {
-		c.JSON(http.StatusInternalServerError,err)
-		return
-	}
-	c.JSON(http.StatusOK,"")
-}
-
